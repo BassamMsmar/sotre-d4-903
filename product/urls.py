796ce92ee9d518c05b6f07cfd9af2636c209391b
bassammsmar <bassammsmar@gmail.com>
2023-09-06 19:41:50 +0300

    add django querysetapi methods

--- product/urls.py
+++ product/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from .views import ProductList, ProductDetail, BrandList, BrandDetails, queryset_depug
+from .views import ProductList, ProductDetail, BrandList, BrandDetails, queryset_debug
 
 urlpatterns = [
     path('', ProductList.as_view(), name='products'),
     path('<str:slug>/', ProductDetail.as_view(), name='product_detail'),
-    path('debug', queryset_depug),
+    path('debug', queryset_debug),
     
     path('brands', BrandList.as_view(), name='brand_list'), 
     path('brands/<str:slug>', BrandDetails.as_view(), name='brand_details'), 
--- product/views.py
+++ product/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from django.db.models import Q , F
-from django.db.models.aggregates import Max,Min,Count,Avg,Sum
-
+from django.db.models import Q , F, Value
+from django.db.models.aggregates import Max, Min, Sum, Avg
 from .models import Product, Review, Brand
+
+
+
+
 
 # Create your views here.
 class ProductList(ListView):
@@ -18,96 +21,6 @@ class ProductDetail(DetailView):
         context = super().get_context_data(**kwargs)
         context["reviews"] = Review.objects.filter(product=self.get_object())
         return context
-    
-
-
-    #data = Product.objects.select_related('brand').all() #prefetch_related = many-to-many 
-    #filter
-    #data = Product.objects.filter(price__gt= 70) #<(__gt)
-    #data = Product.objects.filter(price__gte= 70) #<=(__gte)
-    #data = Product.objects.filter(price__lt= 70) #>(__lt)
-    #data = Product.objects.filter(price__lte= 70) #>=(__lte)
-    #data = Product.objects.filter(price__range = (65,70)) #(__range(x,y))
-    
-    
-    #navigate relation
-    #data = Product.objects.filter(brand__name='Connie Diaz')
-    #data = Product.objects.filter(brand__price__gt=20) #navigate relation with (gt,lt)
-    
-    #filter with text
-    #data = Product.objects.filter(name__contains='Johnson') #search for '....'
-    #data = Product.objects.filter(name__startswith='Danielle') #search for start
-    #data = Product.objects.filter(name__endswith='Novak') #search for end
-    
-    #filter tag
-    #data = Product.objects.filter(tags__isnull=True) #search for tag
-    
-    
-    #filter date & time
-    #data = Review.objects.filter(created_at__year=2023) #search for date & time
-    #data = Review.objects.filter(created_at__month=2023) #search for date & time
-    
-    
-    #filter 2 values
-    #data = Product.objects.filter(price__gt=80,quantity__lt=10) #search 2 values(and)
-    '''data = Product.objects.filter(
-        Q(quantity__lt=10)|
-        Q(price__gt=80)
-    ) #search 2 values(Q   or)'''
-
-    '''data = Product.objects.filter(
-        Q(quantity__lt=10)&
-        Q(price__gt=80)
-    ) #search 2 values(Q   &)'''
-
-    ''' data = Product.objects.filter(
-        Q(quantity__lt=10)|
-        ~Q(price__gt=80)
-    ) #search 2 values(Q   not)~~~~~~~~'''
-
-    #data = Product.objects.filter(price=F('quantity')) #search tower == tower (f)
-    
-    
-    #data = Product.objects.all().order_by('name')#ASC
-    #data = Product.objects.order_by('name')#ASC
-    #data = Product.objects.order_by('name','quantity')#ASC
-    #data = Product.objects.order_by('name').reverse#DES
-    
-    # erorr
-    #data = Product.objects.order_by('name')[0]#first
-    #data = Product.objects.order_by('name')[-1]#last
-
-
-
-    #data = Product.objects.earliest('name')#first
-    #data = Product.objects.latest('name')#last
-    
-    
-    # slice
-    #data = Product.objects.all()[:10]
-    #data = Product.objects.all()[10:20]
-    
-    # select columns
-    #data = Product.objects.values('name','price')
-    #data = Product.objects.values('name','price','brand__name')#dech
-    #data = Product.objects.values_list('name','price','brand__name')#list
-    
-    
-    # remove dublicate
-    #data = Product.objects.all().distinct()
-
-    #data = Product.objects.only('name','price')
-    #data = Product.objects.defer(')
-    
-    # aggregate
-    #data = Product.objects.aggregate(Sum('quantity'))
-    
-    
-    # annotate 
-  
-def  queryset_depug(request):  # data = Product.objects.annotate(price_with_tax=F('price')*1.2) # add new tower
-    date = Product.objects.all()
-    return render(request, 'product/debug.html', {'date':date})
 
 
 
@@ -134,3 +47,94 @@ class BrandDetails(ListView):
         return context
     
     
+
+def queryset_debug(request):
+
+    # date = Product.objects.select_related('brand').all() # prefech_releted : with many-to-many
+
+    # filter
+    # date = Product.objects.filter(price__gt=70)
+    # date = Product.objects.filter(price__lt=70)
+    # date = Product.objects.filter(price__lte=70)
+    # date = Product.objects.filter(price__range=(70,90))
+
+    # navigate relation
+    # date = Product.objects.filter(brand__name='Apple')
+    # date = Product.objects.filter(brand__price__lt=20)
+    # date = Product.objects.filter(brand__price__lt=20)
+
+
+    # filter with text 
+    # date = Product.objects.filter(name__contains='Brown')
+    # date = Product.objects.filter(brand__name__contains='Brown')
+    # date = Product.objects.filter(name__endswith ='e')
+    # date = Product.objects.filter(name__iendswith='e')
+    # date = Product.objects.filter(tags__isnull=False)
+    # date = Product.objects.filter(tags__isnull=True)
+
+    #  fiter date time
+    # date = Review.objects.filter(create_at__year=2023)
+    # date = Review.objects.filter(create_at__month=5)
+
+    # filter 2 values
+    # date = Product.objects.filter(price__gt=80, quantity=10) # and
+    # date = Product.objects.filter(
+    #     Q(price__gt=80) |
+    #     Q(quantity=10)
+    #     ) # or
+    
+    # 
+    
+    # date = Product.objects.filter(
+    #     Q(price__gt=80) &
+    #     ~Q(quantity=10)
+    #     ) # or with not
+
+    # date = Product.objects.filter(price = F('quantity')) # field lookup
+    # date = Product.objects.all().order_by('name')
+    # date = Product.objects.order_by('name')
+
+    # date = Product.objects.all().order_by('-name')
+    # date = Product.objects.order_by('-name') 
+    # date = Product.objects.order_by('price', '-name').reverse() 
+    # date = Product.objects.all().select_related('brand') # prefech_releted : with many-to-many
+    
+
+    # date = Product.objects.order_by('name')[0] 
+    # date = Product.objects.order_by('name')[-1]
+
+    # date = Product.objects.earliest('name')
+    # date = Product.objects.latest('name')
+
+
+    # date = Product.objects.latest('name')
+    # date = Product.objects.latest('name')
+
+
+    
+
+
+    # select columms
+    # date = Product.objects.values('name', 'price')
+    # date = Product.objects.values('name', 'price', 'brand__name')
+    # date = Product.objects.values_list('name', 'price', 'brand__name')
+
+    # remobe duplicate
+    # date = Product.objects.all().distinct()
+    # date = Product.objects.only('name') # اذا كان في الصفحة متغيرات من خارج المطلوب سوف ياخذ وقت اكبر
+    # date = Product.objects.defer('slug') 
+
+
+    # aggregation
+
+    # date = Product.objects.aggregate(Sum('price')) 
+    # date = Product.objects.aggregate(Avg('price')) 
+
+
+    # annotate
+    date = Product.objects.annotate(price_with_tax=F('price')*1.15) 
+    date = Product.objects.annotate(price_with_tax=F('price')*1.15) 
+
+
+
+    return render(request, 'product/debug.html', {'date':date})
